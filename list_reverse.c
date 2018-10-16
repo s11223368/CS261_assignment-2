dcd34bc8bbc1029af3b638b018f76f158d6a2242
Rob Hess <robwhess@gmail.com>
2018-10-16 15:56:04 -0700

    Add complete assignment description and starter code.

---
+++ list_reverse.c
@@ -0,0 +1,28 @@
+/*
+ * This is the file in which you'll write a function to reverse a linked list.
+ * Make sure to add your name and @oregonstate.edu email address below:
+ *
+ * Name:
+ * Email:
+ */
+
+#include <stdio.h>
+
+#include "list_reverse.h"
+
+/*
+ * In this function, you will be passed the head of a singly-linked list, and
+ * you should reverse the linked list and return the new head.  The reversal
+ * must be done in place, and you may not allocate any new memory in this
+ * function.
+ *
+ * Params:
+ *   head - the head of a singly-linked list to be reversed
+ *
+ * Return:
+ *   Should return the new head of the reversed list.  If head is NULL, this
+ *   function should return NULL.
+ */
+struct link* list_reverse(struct link* head) {
+  return NULL;
+}
---
+++ queue_from_stacks.c
@@ -0,0 +1,92 @@
+/*
+ * This is the file in which you'll write the functions required to implement
+ * a queue using two stacks.  Make sure to add your name and @oregonstate.edu
+ * email address below:
+ *
+ * Name:
+ * Email:
+ */
+
+#include <stdio.h>
+
+#include "stack.h"
+#include "queue_from_stacks.h"
+
+/*
+ * This function should allocate and initialize all of the memory needed for
+ * your queue and return a pointer to the queue structure.
+ */
+struct queue_from_stacks* queue_from_stacks_create() {
+  return NULL;
+}
+
+/*
+ * This function should free all of the memory allocated to a queue, including
+ * the memory associated with each stack.
+ *
+ * Params:
+ *   queue - the queue to be destroyed.  May not be NULL.  The function should
+ *     exit the program with an error if queue is NULL.
+ */
+void queue_from_stacks_free(struct queue_from_stacks* queue) {
+
+}
+
+/*
+ * This function should return 0 if a queue is completely empty or 1 if there
+ * is at least one element stored in the queue.
+ *
+ * Params:
+ *   queue - the queue whose emptiness is to be checked.  May not be NULL.
+ *     The function should exit the program with an error if queue is NULL.
+ *
+ * Return:
+ *   Should return 1 if the queue is empty or 0 otherwise.
+ */
+int queue_from_stacks_isempty(struct queue_from_stacks* queue) {
+  return 1;
+}
+
+/*
+ * Should enqueue a new value onto the end of a queue.
+ *
+ * Params:
+ *   queue - the queue onto which to enqueue a value.  May not be NULL.
+ *     The function should exit the program with an error if queue is NULL.
+ *   value - the new value to be enqueueed onto the queue
+ */
+void queue_from_stacks_enqueue(struct queue_from_stacks* queue, int value) {
+
+}
+
+/*
+ * Should return a queue's front value without removing that value from the
+ * queue.
+ *
+ * Params:
+ *   queue - the queue from which to read the front value.  May not be NULL or
+ *     empty.  The function should exit the program with an error if queue is
+ *     NULL or empty.
+ *
+ * Return:
+ *   Should return the value stored at the front of the queue.
+ */
+int queue_from_stacks_front(struct queue_from_stacks* queue) {
+  return 0;
+}
+
+/*
+ * Should remove the front element from a queue and return its value.
+ *
+ * Params:
+ *   queue - the queue from which to dequeue a value.  May not be NULL or
+ *     empty.  The function should exit the program with an error if queue is
+ *     NULL or empty.
+ *
+ * Return:
+ *   Should return the value stored at the front of the queue before that value
+ *   is dequeued.
+ */
+int queue_from_stacks_dequeue(struct queue_from_stacks* queue) {
+  return 0;
+}
